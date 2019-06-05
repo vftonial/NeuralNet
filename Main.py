4f51230b5ff74ba122d092436b44664bc900ba1f
vftonial <tonial900@hotmail.com>
2019-06-04 21:50:05 -0300

    going to sleep, in the middle of something

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-
 # seu valor de ativacao, a lista de pesos que saem dele e a lista de nodos aos quais ele se liga
 # casos especificos para o primeiro e ultimo layer
 class Node:
@@ -11,12 +10,16 @@ class Layer:
     nodes_vector = []
 
 
+class Instances:
+    data = []
+    result = []
+
+
 class NeuralNet:
     input_layer = []
     hidden_layers = [[]]
     output_layer = []
     regularization = 0
-
 
     def backpropagation(self, filename):
         # inicializar os pesos da rede com n zero
@@ -29,7 +32,6 @@ class NeuralNet:
         # avaliar a performance no conjunto de treinamento, se ainda não ta decente roda dnv
         self.read_network(filename)
 
-
     def read_network(self, filename):
         layer = 0
         file = open(filename, "r")
@@ -39,10 +41,10 @@ class NeuralNet:
         for i in range(int(lines[1])):
             self.input_layer.append(Node())
 
-        for line in lines[2:len(lines)-1]: #aparentemente corta antes da ultima entrada
+        for line in lines[2:len(lines) - 1]:  # aparentemente corta antes da ultima entrada
             for i in range(int(line)):
                 self.hidden_layers.append([])
-                self.hidden_layers[layer][i].append(Node()) #certamente esta errado
+                self.hidden_layers[layer][i].append(Node())  # certamente esta errado
             layer = layer + 1
 
         for i in range(int(lines[len(lines)])):
@@ -50,4 +52,12 @@ class NeuralNet:
 
     def read_weights(self, filename):
         file = open(filename, "r")
+        lines = file.readlines()
+        index = 0
+        for line in lines:
+            if index == 0:
 
+
+
+    def read_dataset(self, filename):
+        file = open(filename, "r")
